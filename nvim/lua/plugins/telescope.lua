5ce57415183dc932055c4b1f5c02290b35e22863
Tolu A. <tolu@adesegha.ca>
2026-03-07 09:33:27 -0500

    Formatting

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,10 +5,10 @@ return {
   -- tag = 'v0.1.9',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { "<leader>e", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
-    { '<leader>g', "<cmd>Telescope live_grep<cr>", { desc = 'Telescope live grep' } },
+    { "<leader>e", "<cmd>Telescope find_files<cr>",  desc = "Telescope find files" },
+    { '<leader>g', "<cmd>Telescope live_grep<cr>",   { desc = 'Telescope live grep' } },
     { '<leader>/', "<cmd>Telescope grep_string<cr>", { desc = 'Telescope grep string' } },
-    { '<leader>b', "<cmd>Telescope buffers<cr>", { desc = 'Telescope buffers' } },
+    { '<leader>b', "<cmd>Telescope buffers<cr>",     { desc = 'Telescope buffers' } },
   },
   opts = {
     defaults = {
