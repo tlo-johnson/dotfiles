d7e7751fcc8d2e11c27ff70dd263ae3983f13343
Tolu A. <tolu@adesegha.ca>
2026-05-19 20:04:17 -0400

    disable markdown rendering in neovim

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,5 +1,6 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
+  enabled = false,
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
   opts = {
     latex = {
