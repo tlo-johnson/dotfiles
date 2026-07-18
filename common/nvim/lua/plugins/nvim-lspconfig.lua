d911fbfd27c09331430e9330f329fcbefc4ce892
Tolu A. <tolu@adesegha.ca>
2026-07-18 12:30:09 -0400

    bug fix: nvim lsp

--- common/nvim/lua/plugins/nvim-lspconfig.lua
+++ common/nvim/lua/plugins/nvim-lspconfig.lua
@@ -19,27 +19,4 @@ local masonLspConfig = {
 return {
   "neovim/nvim-lspconfig",
   dependencies = masonLspConfig,
-  config = function()
-    vim.api.nvim_create_user_command("LspRestart", function(opts)
-      local name = opts.args ~= "" and opts.args or nil
-      local clients = name and vim.lsp.get_clients({ name = name }) or vim.lsp.get_clients()
-
-      for _, client in ipairs(clients) do
-        client.stop()
-      end
-
-      vim.defer_fn(function()
-        vim.cmd("edit")
-      end, 200)
-    end, {
-      nargs = "?",
-      complete = function()
-        local names = {}
-        for _, client in ipairs(vim.lsp.get_clients()) do
-          table.insert(names, client.name)
-        end
-        return names
-      end,
-    })
-  end,
 }
