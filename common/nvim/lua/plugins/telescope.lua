a6ba2264daf744b05e7e89f6ff500c7c84297682
Tolu A. <tolu@adesegha.ca>
2026-07-29 02:36:04 -0400

    monkey-patch for opening files with parenthesis in windows

--- common/nvim/lua/plugins/telescope.lua
+++ common/nvim/lua/plugins/telescope.lua
@@ -21,6 +21,38 @@ return {
     },
   },
   config = function(_, opts)
+    local actions = require 'telescope.actions'
+
+    -- https://github.com/MagicDuck/grug-far.nvim/pull/305
+    local is_windows = vim.fn.has('win64') == 1 or vim.fn.has('win32') == 1
+    local vimfnameescape = vim.fn.fnameescape
+    local winfnameescape = function(path)
+      local escaped_path = vimfnameescape(path)
+      if is_windows then
+        local need_extra_esc = path:find('[%[%]`%$~]')
+        local esc = need_extra_esc and '\\\\' or '\\'
+        escaped_path = escaped_path:gsub('\\[%(%)%^&;]', esc .. '%1')
+        if need_extra_esc then
+          escaped_path = escaped_path:gsub("\\\\['` ]", '\\%1')
+        end
+      end
+      return escaped_path
+    end
+
+    local select_default = function(prompt_bufnr)
+      vim.fn.fnameescape = winfnameescape
+      local result = actions.select_default(prompt_bufnr, "default")
+      vim.fn.fnameescape = vimfnameescape
+      return result
+    end
+
+    opts.defaults = vim.tbl_deep_extend('force', opts.defaults or {}, {
+      mappings = {
+        i = { ['<cr>'] = select_default },
+        n = { ['<cr>'] = select_default },
+      },
+    })
+
     require("telescope").setup(opts)
     require("telescope").load_extension("ui-select")
   end,
